5e0bc75fb9b947459e3200dae4473412f3cb1007
Mosses <musicmojica@hotmail.com>
2018-02-06 05:05:56 -0600

    test

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -51,6 +51,8 @@
 
   <!-- jQuery 3 -->
   <script src="vistas/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="vistas/bower_components/jquery-ui/jquery-ui.min.js"></script>
   <!-- Bootstrap 3.3.7 -->
   <script src="vistas/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
   <!-- AdminLTE App -->
