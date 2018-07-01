ead34087a0ed6a7a1ff3d33fdd14349b8c109b9d
mmojica006 <musicmojica@gmail.com>
2018-07-01 14:13:19 -0600

    fix autentication

--- controladores/administradores.controlador.php
+++ controladores/administradores.controlador.php
@@ -10,7 +10,7 @@ class   ControladorAdministradores{
         			if(preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["ingEmail"]) &&
 			   preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])){
 
-  						#$encriptar = crypt($_POST["ingPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+  						$encriptar = crypt($_POST["ingPassword"], '$2y$12$9XMk3whOd9SzIm1/xZaKDerO.86iPSfi6S9sd8KRmTQWwzTFC6CYe');
 
   				$tabla = "administradores";
 				$item = "email";
@@ -18,22 +18,22 @@ class   ControladorAdministradores{
 
 					$respuesta = ModeloAdministradores::mdlMostrarAdministradores($tabla, $item, $valor);
 
-							if($respuesta["email"] == $_POST["ingEmail"] && $respuesta["password"] == $_POST["ingPassword"]){
+                if($respuesta["email"] == $_POST["ingEmail"] && $respuesta["password"] == $encriptar){
 
 					
-					$_SESSION["validarSesionBackend"] = "ok";
-					$_SESSION["id"] = $respuesta["id"];
-					$_SESSION["nombre"] = $respuesta["nombre"];
-					$_SESSION["foto"] = $respuesta["foto"];
-					$_SESSION["email"] = $respuesta["email"];
-					$_SESSION["password"] = $respuesta["password"];
-					$_SESSION["perfil"] = $respuesta["perfil"];
+                            $_SESSION["validarSesionBackend"] = "ok";
+                            $_SESSION["id"] = $respuesta["id"];
+                            $_SESSION["nombre"] = $respuesta["nombre"];
+                            $_SESSION["foto"] = $respuesta["foto"];
+                            $_SESSION["email"] = $respuesta["email"];
+                            $_SESSION["password"] = $respuesta["password"];
+                            $_SESSION["perfil"] = $respuesta["perfil"];
 
-					echo '<script>
-
-						window.location = "bienvenidos";
-
-					</script>';
+                            echo '<script>
+        
+                                window.location = "bienvenidos";
+        
+                            </script>';
 
 				}else{
 
@@ -42,17 +42,18 @@ class   ControladorAdministradores{
 
 				}
 
+        			}else{
+                        echo '<br>
+					<div class="alert alert-danger">Error validando credenciales</div>';
 
-
-
-
-
-        			}
+                    }
             
             
             
             
         }
+
+
         
     }
     
